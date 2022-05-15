354632c2d7d9b5cdfea9997deeb39fdb0ad7e052
ma-chang <akihisa.bamba.10@gmail.com>
2022-05-15 12:41:24 +0900

    implement core component with app layout

---
+++ src/features/core/Core.tsx
@@ -0,0 +1,199 @@
+import React, { useEffect } from 'react';
+import Auth from '../auth/Auth';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../../app/store';
+
+import styles from './Core.module.css';
+import { withStyles } from '@material-ui/core/styles';
+import { Avatar, Badge, Button, CircularProgress, Grid } from '@material-ui/core';
+import { MdAddAPhoto } from 'react-icons/md';
+
+import {
+  editNickname,
+  resetOpenProfile,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  selectIsLoadingAuth,
+  selectProfile,
+  setOpenProfile,
+  fetchAsyncGetMyProfile,
+  fetchAsyncGetProfiles,
+} from '../auth/authSlice';
+
+import {
+  resetOpenNewPost,
+  selectIsLoadingPost,
+  selectPosts,
+  setOpenNewPost,
+  fetchAsyncGetPosts,
+  fetchAsyncGetComments,
+} from '../post/postSlice';
+
+// import Post from '../post/Post';
+// import EditProfile from './EditProfile';
+// import NewPost from './NewPost';
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}))(Badge);
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//     '& > *': {
+//       margin: theme.spacing(1),
+//     },
+//   },
+// }));
+
+const Core: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const profile = useSelector(selectProfile);
+  const posts = useSelector(selectPosts);
+  const isLoadingAuth = useSelector(selectIsLoadingAuth);
+  const isLoadingPost = useSelector(selectIsLoadingPost);
+
+  useEffect(() => {
+    const fetchBootLoader = async () => {
+      if (localStorage.localJWT) {
+        dispatch(resetOpenSignIn());
+        const result = await dispatch(fetchAsyncGetMyProfile());
+        if (fetchAsyncGetMyProfile.rejected.match(result)) {
+          dispatch(setOpenSignIn());
+          return null;
+        }
+        await dispatch(fetchAsyncGetPosts());
+        await dispatch(fetchAsyncGetProfiles());
+        await dispatch(fetchAsyncGetComments());
+      }
+    };
+    fetchBootLoader();
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Auth />
+      <div className={styles.core_header}>
+        <h1 className={styles.core_title}>SNS Clone</h1>
+        {profile?.nickName ? (
+          <>
+            <button
+              className={styles.core_btnModal}
+              onClick={() => {
+                dispatch(setOpenNewPost());
+                dispatch(setOpenProfile());
+              }}
+            >
+              <MdAddAPhoto />
+            </button>
+            <div className='core_logout'>
+              {(isLoadingPost || isLoadingAuth) && <CircularProgress />}
+              <Button
+                onClick={() => {
+                  localStorage.removeItem('localJWT');
+                  dispatch(editNickname(''));
+                  dispatch(resetOpenProfile());
+                  dispatch(resetOpenNewPost());
+                  dispatch(setOpenSignIn());
+                }}
+              >
+                Logout
+              </Button>
+              <button
+                className={styles.core_btnModal}
+                onClick={() => {
+                  dispatch(setOpenProfile());
+                  dispatch(resetOpenNewPost());
+                }}
+              >
+                <StyledBadge
+                  overlap='circular'
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  variant='dot'
+                >
+                  <Avatar alt='who?' src={profile.img} />
+                </StyledBadge>
+              </button>
+            </div>
+          </>
+        ) : (
+          <div>
+            <Button
+              onClick={() => {
+                dispatch(setOpenSignIn());
+                dispatch(resetOpenSignUp());
+              }}
+            >
+              LogIn
+            </Button>
+            <Button
+              onClick={() => {
+                dispatch(setOpenSignUp());
+                dispatch(resetOpenSignIn());
+              }}
+            >
+              SignUp
+            </Button>
+          </div>
+        )}
+      </div>
+
+      {profile?.nickName && (
+        <>
+          <div className={styles.core_posts}>
+            <Grid container spacing={4}>
+              {posts
+                .slice(0)
+                .reverse()
+                .map((post) => (
+                  <Grid key={post.id} item xs={12} md={4}>
+                    <Post
+                      postId={post.id}
+                      title={post.title}
+                      loginId={profile.userProfile}
+                      userPost={post.userPost}
+                      imageUrl={post.img}
+                      liked={post.liked}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Core;
